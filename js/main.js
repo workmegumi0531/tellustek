43a59134b2be0fb3d7e4c3bef08def1e2ce5d25c
workmegumi0531 <73944105+workmegumi0531@users.noreply.github.com>
2020-12-02 22:44:54 +0800

    updata
    chansge RWD , add ajax

--- js/main.js
+++ js/main.js
@@ -98,7 +98,6 @@ $(function () {
   var timer = null;
   var iNow = 0;
   var play_screen = $(window).width();
-
   aBtn3
     .stop()
     .eq(0)
@@ -107,7 +106,6 @@ $(function () {
     iNow = $(this).index();
     tab();
   });
-
   $("#play,#menu")
     .mouseenter(function () {
       clearInterval(timer);
@@ -118,12 +116,10 @@ $(function () {
         tab();
       }, 4000);
     });
-
   timer = setInterval(function () {
     iNow++;
     tab();
   }, 4000);
-
   function tab() {
     aBtns.attr("class", "").eq(iNow).attr("class", "active");
     aBtn2.css({ color: "#aaa" });
@@ -157,4 +153,94 @@ $(function () {
       });
     }
   }
+});
+$(function () {
+  sc_num();
+  $.ajax({
+    url: "img_title.json",
+    dataType: "json",
+    success: function (arr) {
+      for (i = 0; i < arr.length; i++) {
+        var B_m_bottom = $(
+          ` <div id="B_m_item">
+              <img src="${arr[i].img}" alt="" width="330" />
+              <div id="B_m_p_detail">
+                <p>${arr[i].price}</p>
+                <h3>${arr[i].title}</h3>
+                <div id="B_m_icon_item">
+                  <i class="icon_click far fa-eye" id="i_eye"></i>
+                  <i class="icon_click far fa-star" id="i_st"></i>
+                  <i class="icon_click fas fa-share-square" id="i_ss"></i>
+                  <i class="icon_click fas fa-shopping-cart" id="${arr[i].id}"></i>
+                  <p class="o_o_s_n">OUT OF STOCK</p>
+                </div>
+              </div>
+            </div>`
+        );
+        B_m_bottom.appendTo("#B_m_bottom");
+      }
+    },
+    error: function (msg) {
+      console.log(msg);
+    },
+  });
+
+  $("#B_m_bottom").on("click", "#i_eye", function () {
+    alert("已成功關注");
+  });
+  $("#B_m_bottom").on("click", "#i_st", function () {
+    alert("已加入最愛");
+  });
+  $("#B_m_bottom").on("click", "#i_ss", function () {
+    alert("已分享成功");
+  });
+  $("#B_m_bottom").on("click", ".fa-shopping-cart", function () {
+    var id = this.id;
+    var first = $.cookie("products") == null ? true : false;
+
+    if (first) {
+      var arr = [{ id: id, num: 1 }];
+      $.cookie("products", JSON.stringify(arr), {
+        expires: 7,
+      });
+    } else {
+      var cookieStr = $.cookie("products");
+      var cookieArr = JSON.parse(cookieStr);
+      var same = false;
+
+      for (var i = 0; i < cookieArr.length; i++) {
+        if (id == cookieArr[i].id) {
+          same = true;
+          cookieArr[i].num++;
+          break;
+        }
+      }
+      if (!same) {
+        cookieArr.push({ id: id, num: 1 });
+      }
+      $.cookie("products", JSON.stringify(cookieArr), {
+        expires: 7,
+      });
+    }
+    sc_num();
+    alert("已成功加入購物車");
+  });
+  function sc_num() {
+    var cookieStr = $.cookie("products");
+    if (cookieStr) {
+      var cookieArr = JSON.parse(cookieStr);
+      var sum = 0;
+      for (var i = 0; i < cookieArr.length; i++) {
+        sum = sum + cookieArr[i].num;
+      }
+      $("#sc_num").html(sum);
+    } else {
+      $("#sc_num").html(0);
+    }
+  }
+});
+$(function () {
+  $("#addon_item_3").on("hover", function () {
+    $("#span").css({ "font-weight": 900 });
+  });
 });
